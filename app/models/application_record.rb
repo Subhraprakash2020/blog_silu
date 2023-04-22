fb27b09e5b1238b6e12a98802488a472d8572957
Subhraprakash Subudhi <subhra.techy@gmail.com>
2023-04-22 19:39:41 +0530

    Add the authentication system
     - Add authentication system in blog application

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  validates:title, presence:true
-  validates:content, presence:true,length: {minimum:500}
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates:title, presence:true
+  validates:content, presence:true,length: {minimum:500}
 end
