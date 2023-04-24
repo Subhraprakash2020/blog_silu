847e4d2be2b2d901bb192b5f239614f3d5f41afe
Subhraprakash Subudhi <subhra.techy@gmail.com>
2023-04-24 12:00:58 +0530

    Add authentication system
     - Add authentication system to the blog application.

--- db/schema.rb
+++ db/schema.rb
@@ -55,4 +55,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_054828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
