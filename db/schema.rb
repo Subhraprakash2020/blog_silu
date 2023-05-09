5a78c19bbf8c478c1f63c791bf71616212cb5a85
Subhraprakash Subudhi <subhra.techy@gmail.com>
2023-05-09 15:44:05 +0530

    Revert commit
    This reverts commit eca42763d9f5ba1de135540a839dc8afb41259f8.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_069998) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_054828) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_069998) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.integer "tag_id"
   end
 
   create_table "posts_tags", id: false, force: :cascade do |t|
