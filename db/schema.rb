5304a96d4e300013592984b6b164b76fbab348c3
Subhraprakash Subudhi <subhra.techy@gmail.com>
2023-05-25 14:35:16 +0530

    Deploy blog app to heroku
     - Change the database from sqlite3 to PostgreSql.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_034355) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_173707) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -57,15 +60,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_034355) do
   end
 
   create_table "categories_posts", id: false, force: :cascade do |t|
-    t.integer "category_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "post_id", null: false
     t.index ["category_id", "post_id"], name: "index_categories_posts_on_category_id_and_post_id"
     t.index ["post_id", "category_id"], name: "index_categories_posts_on_post_id_and_category_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -78,8 +76,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_034355) do
   end
 
   create_table "posts_tags", id: false, force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
     t.index ["post_id", "tag_id"], name: "index_posts_tags_on_post_id_and_tag_id"
     t.index ["tag_id", "post_id"], name: "index_posts_tags_on_tag_id_and_post_id"
   end
@@ -87,11 +85,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_034355) do
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trips", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
